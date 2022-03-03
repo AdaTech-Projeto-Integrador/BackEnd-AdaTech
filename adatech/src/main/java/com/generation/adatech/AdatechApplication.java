ba20b0a19dfe771f83a8a890b7948504ff54ca21
Igorluan95 <igorluansouzasilva@gmail.com>
2022-03-03 13:38:26 -0300

    Maven Protect com as dependências necessárias para o Projeto Integrador

---
+++ adatech/src/main/java/com/generation/adatech/AdatechApplication.java
@@ -0,0 +1,13 @@
+package com.generation.adatech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdatechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdatechApplication.class, args);
+	}
+
+}
---
+++ adatech/src/test/java/com/generation/adatech/AdatechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.adatech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdatechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
